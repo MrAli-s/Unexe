7e2e9953457675ea805e01a1ae9ad9d67cde0bfc
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2021-10-09 08:16:16 -0500

    Create unexe.py

---
+++ unexe.py
@@ -0,0 +1,12 @@
+import os
+import sys
+os.system("pip3 install -r requirements.txt")
+os.system("apt install git && apt install python3 -y")
+os.system("git clone https://github.com/rocky/python-decompile3")
+os.getcwd()
+os.chdir("python-decompile3")
+os.system("python3 setup.py install")
+os.chdir("../")
+run = input("Your File: ")
+os.system("python3 main" + run)
+print("Done :D")
